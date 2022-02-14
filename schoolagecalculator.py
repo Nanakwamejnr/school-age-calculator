462831cbc5f0ff2d2ce43b45a255ccc1a8198c12
Nanakwamejnr <jamponsah550@gmail.com>
2022-02-14 05:18:21 +0000

    school age calculator

---
+++ schoolagecalculator.py
@@ -0,0 +1,30 @@
+print("School age calculator")
+name = input("type your name : ")
+age = int(input("type your age : "))
+
+if age <5:
+    print(name,"should be in nursery")
+if age ==5:
+    print(name,"should be in kindergarten 1")
+if age ==6:
+    print(name,"should be in kindergarten 2")
+if age ==7:
+    print(name,"should be in class 1")
+if age ==8:
+    print(name,"should be in class 2")
+if age ==9:
+    print(name,"should be in class 3")
+if age ==10:
+    print(name,"should be in class 4")
+if age ==11:
+    print(name,"should be in class 5")
+if age ==12:
+    print(name,"should be in class 6")
+if age == 13:
+    print(name,"should be in JHS 1")
+if age ==14:
+    print(name,"should be in JHS 2")
+if age ==15:
+    print(name,"should be in JHS3")
+if age >15:
+    print(name,"please you can,t be admitted in this school")
